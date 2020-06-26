d58b553afaad043219aa2cb3602dc2644a521b8f
staya-apurup <67386060+staya-apurup@users.noreply.github.com>
2020-06-26 18:58:06 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,41 @@
+a.sin 60
+import math
+sine=math.sin(60)
+print(sine)
+
+b.cos(pi)
+import math
+co=math.cos(pi)
+print(co)
+
+c.tan(90)
+import math
+ta=math.tan(90)
+print(ta)
+
+d.angle of sin(0.8660254037844386)
+import math
+print(math.sin((0.86602540378443860009)))
+
+e.5^8
+print(5**8)
+
+f.Square root of 400
+print(400**0.5)
+
+g.The value of 5^e
+import math
+print(5*(math.e))
+
+h.The value of Log(1024), base 2
+import math
+print(math.log2(1024))
+
+i.The value of Log(1024), base 10
+import math
+print(math.log10(1024))
+
+j.The Floor and Ceiling value of 23.56
+import math
+print(math.floor(23.56))
+print(math.ceil(23.56))
