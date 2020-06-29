7d6ff67dadb08cd2b659a2ce2c0c5912e8c48437
staya-apurup <67386060+staya-apurup@users.noreply.github.com>
2020-06-29 13:28:16 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,4 @@
+b=int(input("breadth is")) 
+h=int(input("height is"))
+A=0.5*b*h 
+print("area of triangle",A)
---
+++ circumference of circle.py
@@ -0,0 +1,4 @@
+r=float(input("radius of the circle:")) 
+pi=3.142 
+C=2*pi*r 
+print("circumferenceof a circle is:",C)
